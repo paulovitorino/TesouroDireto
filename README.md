94bb4e80626000ab2b86860548c2096af7da65db
Paulo Vitorino <pavf.paulo@gmail.com>
2021-06-21 16:06:12 -0300

    Projeto funcional

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# TesouroDireto
-API for new functions on Google Sheets about Brazilian Government Bonds (Tesouro Direto)
+<h1 align="center">Tesouro Direto</h1>
+![Badge](https://img.shields.io/badge/Made%20with-JavaScript-%23F7df1e?style=plastic&logo=javascript)
+![Badge](https://img.shields.io/badge/Made%20for-Google%20Sheets-%2334a853?style=plastic&logo=	googlesheets)
+<h2 align="center" id="about">About</h2>
+<p align="center">API for new functions on Google Sheets about Brazilian Government Bonds (Tesouro Direto).</p>
+<h2 align="center" id="table-of-contents">Table of Contents</h2>
+=================
+<!--ts-->
+   * [About](#about)
+   * [Table of Contents](#table-of-contents)
+   * [Project Status](#project-status)
+<!--te-->
+
+<h2 align="center" id="project-status">Project Status</h2>
+[![Project Status: Active – The project has reached a stable, usable state and is being actively developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
--- tesouro.js
+++ tesouro.js
@@ -1,19 +1,48 @@
 /*
-* @return Retorns data from Brazilian Government Bonds: Name, Reference Date.
+* @return Returns data from Brazilian Government Bonds: Name, Withdraw Value, Withdraw Date, Reference Date.
 * @customfunction
 **/
-function TesouroDireto() {
+function TesouroDireto(valores) {
     let srcURL = "https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json";
     let jsonData = UrlFetchApp.fetch(srcURL);
     let parsedData = JSON.parse(jsonData.getContentText()).response;
-    
+
+    valores = valores.split(" ").join("");
+    valores = valores.split(";").join(",");
+    const partArray = valores.split(",");
+
     let atualizacao = new Date(parsedData.TrsrBondMkt.qtnDtTm);
     let cont = 0;
-    const valoresArray = [];
+    var valoresArray = [];
+    var linha = [];
+    let contLinha = 0;
 
-    for(let bond of parsedData.TrsrBdTradgList) {
-        valoresArray[cont] = [bond.TrsrBd.nm, atualizacao];
+    for (let bond of parsedData.TrsrBdTradgList) {
+        for (let part of partArray) {
+            linha[contLinha] = RetornaPadrao(bond, part, atualizacao);
+            contLinha++;
+        }
+        valoresArray[cont] = Array.from(linha);
         cont++;
+        contLinha = 0;
     }
     return valoresArray;
+}
+/*
+* @return Returns values defined by B3 in JSON file for TesouroDireto function.
+* @customfunction
+**/
+function RetornaPadrao(source, part, atual) {
+    switch (part) {
+        case "nome":
+            return source.TrsrBd.nm;
+        case "dataref":
+            return atual;
+        case "valor":
+            return source.TrsrBd.untrRedVal;
+        case "datafim":
+            return new Date(source.TrsrBd.mtrtyDt);
+        default:
+            return "#ERROR";
+    }
 }
